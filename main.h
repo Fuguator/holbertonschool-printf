93a7818937acb6dcff18e45cef3f0637d08cc584
Your Name <you@example.com>
2024-07-24 15:48:33 +0400

    commit

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <unistd.h>
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
