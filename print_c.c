700fc9c1acbef2b207c05c4c5f8186d11ca8df82
Your Name <you@example.com>
2024-07-27 23:04:40 +0400

    commit

--- print_c.c
+++ print_c.c
@@ -10,7 +10,7 @@ int print_c(int *len, char a)
 	if (!a)
 		write(1, "(null)", 6), *len += 6;
 	else if (a == '\0')
-		return;
+		return (*len);
 	else
 		write(1, &a, 1), (*len)++;
 	return (*len);
