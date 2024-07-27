94279ba819ea458d965c27505e4ddd933d33d691
Your Name <you@example.com>
2024-07-27 21:29:25 +0400

    commit

--- _putchar.c
+++ _putchar.c
@@ -1,10 +1,8 @@
-#include <unistd.h>
+#include "main.h"
 /**
- * _putchar - Writes a character to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _putchar - prints a character
+ * @c: value in buffer
+ * Return: value from buffer
  */
 int _putchar(char c)
 {
