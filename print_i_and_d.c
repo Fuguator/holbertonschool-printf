fdb9eeb86272884674ecbcf87da4c360e17ce65e
Your Name <you@example.com>
2024-07-27 22:50:40 +0400

    commit

--- print_i_and_d.c
+++ print_i_and_d.c
@@ -1,29 +1,28 @@
 #include "main.h"
 /**
- * 
+ * print_i_and_d - prints int and decimals
+ * @len: length of string
+ * @a: int/d
+ * Return: length
  */
 int print_i_and_d(int *len, va_list a)
 {
-    int i = 1, num = va_arg(a, int);
+	int i = 1, num = va_arg(a, int);
 
-    if(num < 0)
-    {
-        _putchar('-');
-        num = -num;
-        (*len)++;
-    }
-    for(; (num / i) / 10 != 0; i *= 10)
-    {
-        (*len)++;
-    }
-    for(; i != 1; i /= 10)
-    {
-        _puthcar('0' + (num / i));
-        num %= i;
-    }
-    _puthcar('0' + (num % 10));
-    (*len)++;
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+		(*len)++;
+	}
+	for (; (num / i) / 10 != 0; i *= 10)
+		(*len)++;
+	for (; i != 1; i /= 10)
+		_puthcar('0' + (num / i)), num %= i;
 
-    va_end(a);
-    return (*len);
+	_putchar('0' + (num % 10));
+	(*len)++;
+
+	va_end(a);
+	return (*len);
 }
