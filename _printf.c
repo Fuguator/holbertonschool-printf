3eb1d328a64f0b200884b7d9a30074aca28f4871
Your Name <you@example.com>
2024-07-27 22:18:47 +0400

    commit

--- _printf.c
+++ _printf.c
@@ -1,69 +1,61 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
 /**
- * _printf - Produces output according to a format
- * @format: Is a character string. The format string
- * is composed of zero or more directives
- *
- * Return: The number of characters printed (excluding
- * the null byte used to end output to strings)
- **/
+ * 
+ */
 int _printf(const char *format, ...)
 {
+	int i = 0, len = 0;
 	va_list arr;
-	int i = 0, j, len = 0;
-
+	
 	va_start(arr, format);
 
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%' && format[i + 1] == 'c')
-		{
-			_putchar(va_arg(arr, int));
-			i += 2;
-			len++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 's')
-		{
-			char *str = va_arg(arr, char *);
-			if (str == NULL)
-			{
-				_putchar('(');
-				_putchar('n');
-				_putchar('u');
-				_putchar('l');
-				_putchar('l');
-				_putchar(')');
-				len += 6;
-				i += 2;
-			}
-			else
-			{
-				for (j = 0; str[j] != '\0'; j++)
-				{
-					_putchar(str[j]);
-					len++;
-				}
-				i += 2;
-			}
-		}
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			_putchar('%');
-			i += 2;
-			len++;
-		}
-		else if (format[i] == '%'  && format[i + 1] == '\0')
-		{
-			_putchar('%');
-			len++;
-			i++;
-		}
-		else
-			_putchar(format[i]), len++, i++;
-	}
-	va_end(arr);
-	return (len);
+    if(!format)
+    {
+        return (-1);
+    }
+
+    for (; *(format + i) != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == 'c')
+            {
+                print_c(arr, &len);
+            }
+            else if(format[i + 1] == 's')
+            {
+                print_s(arr, &len);
+            }
+            else if(format[i + 1] == 'i' || format[i + 1] == 'd')
+            {
+                print_i(arr, &len);
+            }
+            else if(format[i + 1] == '%')
+            {
+                _putchar('%'), len++;
+            }
+            else if(format[i + 1] == '\0')
+            {
+                continue;
+            }
+            else
+            {
+                _putchar('%');
+                _putchar(format[i + 1]);
+                len += 2;
+            }
+            i++;
+        }
+        else
+        {
+            _putchar(format[i]), len++;
+        }
+    }
+
+    if (!len || len == 0)
+    {
+        return (-1);
+    }
+    va_end(arr);
+    return (len);
 }
