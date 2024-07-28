530d28fab23a33a00cbbb23c2078de76c78f9110
Your Name <you@example.com>
2024-07-28 13:12:56 +0400

    commit

--- print_i_and_d.c
+++ print_i_and_d.c
@@ -7,10 +7,11 @@
  */
 int print_i_and_d(int *len, va_list a)
 {
-	unsigned int i = 1, num = va_arg(a, int);
+	unsigned int i = 1;
+	int num = va_arg(a, int);
 
 	if (num < 0)
-	{
+	{    
 		_putchar('-');
 		num = -num;
 		(*len)++;
