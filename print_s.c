5e5f29153d1a78a1417c0b446b6b2e82160b3883
root <you@example.com>
2024-07-28 16:19:23 +0000

    commit

--- print_s.c
+++ print_s.c
@@ -3,24 +3,24 @@
  * print_s - prints a string
  * @len: length of string
  * @a: first element of string
- * Retutn: length
+ * Return: length
  */
 int print_s(int *len, char *a)
 {
-    int i = 0;
+	int i = 0;
 
-    if(!a)
-    {
-        write(1, "(null)", 6);
-        *len += 6;
-    }
-    else
-    {
-        for (; *(a + i) != '\0'; i++)
-        {
-            write(1, (a + i), 1);
-            (*len)++;
-        }
-    }
-    return (*len);
+	if (!a)
+	{
+		write(1, "(null)", 6);
+		*len += 6;
+	}
+	else
+	{
+		for (; *(a + i) != '\0'; i++)
+		{
+			write(1, (a + i), 1);
+			(*len)++;
+		}
+	}
+	return (*len);
 }
